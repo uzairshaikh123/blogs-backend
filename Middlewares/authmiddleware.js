664a9251b9c7c312af93c17d747f9a0c5aae0766
Uzair <uzairmd073@gmail.com>
2023-05-13 16:07:38 +0530

    sum fixes

--- Middlewares/authmiddleware.js
+++ Middlewares/authmiddleware.js
@@ -9,6 +9,7 @@ let token = req.headers.authorization
 
     jwt.verify(token, 'user', function(err, decoded) {
         if(decoded){
+            req.body.userID=decoded.userID
             next()
         }else{
             res.send({"msg":"It is a Private route and User is not authenticated we need a token for authenticate"})
